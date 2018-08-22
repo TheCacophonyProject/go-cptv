1382c1bedeb7bb6d1fe0718f236f89bd5b7dea80
Simon Matthews <simonm555@hotmail.com>
2018-08-22 22:20:14 +1200

    fix cptvtool before updating it to work with the new Reader

--- cptvtool/main.go
+++ cptvtool/main.go
@@ -1,5 +1,19 @@
 package main
 
+// Copyright 2018 The Cacophony Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.package main
+
 import (
 	"bufio"
 	"fmt"
@@ -36,22 +50,42 @@ func runMain() error {
 		return err
 	}
 
-	fmt.Println(fields.Timestamp(cptv.Timestamp))
-	fmt.Println(fields.Uint32(cptv.XResolution))
-	fmt.Println(fields.Uint32(cptv.YResolution))
-	fmt.Println(fields.Uint8(cptv.Compression))
-	fmt.Println(fields.String(cptv.DeviceName))
+	ts, err := fields.Timestamp(cptv.Timestamp)
+	fmt.Println("Timestamp:   ", ts, err)
+	xres, err := fields.Uint32(cptv.XResolution)
+	fmt.Println("X Resolution:", xres, err)
+	yres, err := fields.Uint32(cptv.YResolution)
+	fmt.Println("YResolution: ", yres, err)
+	cmpr, err := fields.Uint8(cptv.Compression)
+	fmt.Println("Compression: ", cmpr, err)
+	devc, err := fields.String(cptv.DeviceName)
+	fmt.Println("DeviceName: ", devc, err)
 
 	frames := 0
 	for {
-		_, _, err := r.Frame()
+		fields, frameReader, err := r.Frame()
+
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			return err
 		}
+
+		// skip past the frame
+		frameLen, err := fields.Uint32(cptv.FrameSize)
+		buf := make([]byte, frameLen)
+		bytesLeft := frameLen
+		for frameLen > 0 {
+			n, err := frameReader.Read(buf)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return err
+			}
+			bytesLeft = bytesLeft - uint32(n)
+		}
 		frames++
 	}
-	fmt.Println("frames:", frames)
+	fmt.Println("Frames:      ", frames)
 	return nil
 }
